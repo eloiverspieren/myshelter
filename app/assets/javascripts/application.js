98738c0ac794cef58dd55ebb0c2dae5051237d43
arrabatur <fix@doum.it>
2016-08-23 16:33:35 +0200

    setup theme, home, login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,19 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require jquery.counterup.min
+//= require jquery.selectbox-0.1.3.min
+//= require isotope.min
+//= require isotope-triger
+//= require jquery.fancybox.pack
+//= require bootstrap-datepicker
+//= require isotope-triger
+//= require jquery.syotimer
 //= require bootstrap-sprockets
-//= require_tree .
+//= require jquery.themepunch.revolution.min
+//= require jquery.themepunch.tools.min
+
+//= require jquery.themepunch.enablelog
+//= require jquery.themepunch.revolution
+
+//= require custom
