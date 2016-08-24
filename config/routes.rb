0eff1051b5bd86b88a079869ef0e3ecf8f253edb
eloi verspieren <eloi.verspieren@gmail.com>
2016-08-24 11:42:53 +0200

    bookaccept

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,15 @@ Rails.application.routes.draw do
     resources :bookings
   end
   resource :profiles
+
   get 'my_bookings', to: 'bookings#my_bookings'
+  resources :bookings, only: [:index] do
+    member do
+      get 'accept'
+      get 'reject'
+    end
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
